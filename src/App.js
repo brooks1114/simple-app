dc36bece508b6f86c0916ad3626b0303cc0e4934
brooks1114 <58515991+brooks1114@users.noreply.github.com>
2022-07-25 22:17:14 -0400

    Update App.js
    testing the app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and Is github app working?.
+          Edit <code>src/App.js</code> and Is github app work?.
         </p>
         <a
           className="App-link"
